48586b776cef1e70344b82be21cb4f78af3d93ce
Athena Hernández <64385980+athenachernandez@users.noreply.github.com>
2020-05-09 11:27:28 -0700

    LeagueInvaders

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,29 @@
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
-
+	JFrame frame;
+	public static final int WIDTH = 500;
+	public static final int HEIGHT = 800;
+	GamePanel panel;
+	
+	public LeagueInvaders() {
+		frame = new JFrame();
+		panel = new GamePanel();
+	}
+	
+	void setup() {
+		frame.add(panel);
+		frame.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.addKeyListener(panel);
+		frame.pack();
+	}
+	
+	public static void main(String[] args) {
+		LeagueInvaders invaders = new LeagueInvaders();
+		invaders.setup();
+	}
 }
